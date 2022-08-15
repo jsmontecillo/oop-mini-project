63ec8a8e51ba54508461820416bfdc1a6ddec680
jsmontecillo <jsmontecillo@gmail.com>
2022-08-15 10:02:10 -0700

    added event objects

---
+++ event.js
@@ -0,0 +1,42 @@
+class Event {
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
+        this.availableTickets = [];
+    }
+}
+
+const eventObj1 = new Event (
+    'KLOS Golden Gala',
+    'An evening with hollywood vampires'
+);
+
+const eventObj2 = new Event (
+    'Skillet & Sevendust',
+    'Victorious war tour'
+)
+
+const eventObj3 = new Event(
+    'Jenny Lewis',
+    'On the line tour 2019'
+)
+
+const eventArray = new Array();
+
+eventArray.push(eventObj1, eventObj2, eventObj3);
+console.log(eventArray);
+
+document.addEventListener('DOMContentLoaded', () => {
+    let html = '';
+    eventArray.forEach((item) => {
+        html += `<li>${item.name} - ${item.description}`; 
+    });
+    document.querySelector('#event').innerHTML = html;
+});
+
+class TicketType {
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+}
